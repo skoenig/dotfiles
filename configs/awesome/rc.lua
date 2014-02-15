683ff2cfbfbfd4727141d24274ac7affba357d1e
skoenig <soeren.koenig@mailbox.org>
2014-02-15 18:58:33 +0100

    awesomewm: added awesome theme switcher

--- configs/awesome/rc.lua
+++ configs/awesome/rc.lua
@@ -58,7 +58,32 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init(awful.util.getdir("config") .. "/themes/nice-and-clean-theme/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/current_theme/theme.lua")
+mythememenu = {}
+
+function theme_load(theme)
+   local cfg_path = awful.util.getdir("config")
+
+   -- Create a symlink from the given theme to /home/user/.config/awesome/current_theme
+   awful.util.spawn("ln -sfn " .. cfg_path .. "/themes/" .. theme .. " " .. cfg_path .. "/current_theme")
+   awesome.restart()
+end
+
+function theme_menu()
+   -- List your theme files and feed the menu table
+   local cmd = "ls -1 " .. awful.util.getdir("config") .. "/themes/"
+   local f = io.popen(cmd)
+
+   for l in f:lines() do
+      local item = { l, function () theme_load(l) end }
+      table.insert(mythememenu, item)
+   end
+
+   f:close()
+end
+
+-- Generate your table at startup or restart
+theme_menu()
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
@@ -235,7 +260,7 @@ local  white                 = "#ffffff"
 theme.font          = "monospace 8"
 theme.bg_focus      = "#3465A4"
 theme.border_width  = "2"
-theme.border_focus  = mediumspringgreen
+theme.border_focus  = lightsalmon
 --theme.bg_focus      = springgreen
 -- }}}
 
@@ -253,6 +278,7 @@ end
 myawesomemenu = {
    { "manual", terminal .. " -e man awesome" },
    { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "themes", mythememenu },
    { "restart", awesome.restart },
    { "quit", awesome.quit }
 }
