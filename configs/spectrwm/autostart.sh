06f23d33992a901b0ebc5455e35e826e169e168d
skoenig <soeren.koenig@mailbox.org>
2015-07-05 14:12:19 +0200

    normalized spectrwm config

--- configs/spectrwm/autostart.sh
+++ configs/spectrwm/autostart.sh
@@ -1,36 +1,25 @@
-## GNOME PolicyKit and Keyring
-eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
+#!/bin/bash
+
+# mimic a non-reparenting window manager for Java applications
+wmname LG3D
 
 ## Set root window colour
 hsetroot -solid "#333333" &
-feh --bg-fill ~/graphic/wallpapers/nature/nature_190.png & 
+feh --bg-fill ~/graphic/wallpapers/nature/nature_190.png &
 
-## Volume keys daemon
-#xfce4-volumed &
+## this may be also needed by nm-applet
+#/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
-## Enable power management
-#xfce4-power-manager &
-
-## Detect and configure touchpad. See 'man synclient' for more info.
-if egrep -iq 'touchpad' /proc/bus/input/devices; then
-    synclient VertEdgeScroll=1 &
-    synclient TapButton1=1 &
-fi
-
-## Start screensaver
-gnome-screensaver &
-
-## Set keyboard settings - 250 ms delay and 25 cps (characters per second) repeat rate.
-## Adjust the values according to your preferances.
-xset r rate 250 25 &
-
-## Turn on/off system beep
-xset b off &
-
-trayer --edge top --align left --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x333333 --height 16 --width 4 & 
-
-nm-applet &
-pidgin &
+## Start daemons
 ~/.dropbox-dist/dropboxd &
+trayer --edge top --align left --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x333333 --height 16 --width 4 &
 
-exec spectrwm  
+cd ~
+
+## GNOME PolicyKit and Keyring
+eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
+export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+#eval $(ssh-agent -s)
+#export SSH_AUTH_SOCK SSH_AGENT_PID
+
+exec spectrwm
