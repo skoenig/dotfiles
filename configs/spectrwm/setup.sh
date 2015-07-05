85066c34c26e56f69ae3c6c9401f9d5eaa11a727
skoenig <soeren.koenig@mailbox.org>
2015-07-05 17:36:14 +0200

    use latest spectrwm version

--- configs/spectrwm/setup.sh
+++ configs/spectrwm/setup.sh
@@ -6,9 +6,9 @@ sudo apt-get -y install "^libxcb*..*-dev$" feh gnome-keyring libx11-xcb-dev libx
 libxt-dev network-manager-gnome sakura scrot suckless-tools trayer xfce4-power-manager xterm
 
 cd /tmp
-wget https://github.com/conformal/spectrwm/archive/SPECTRWM_2_3_0.tar.gz
-tar xf SPECTRWM_2_3_0.tar.gz
-cd spectrwm-SPECTRWM_2_3_0/linux/ && make
+wget https://github.com/conformal/spectrwm/archive/SPECTRWM_2_7_2.tar.gz
+tar xf SPECTRWM_2_7_2.tar.gz
+cd spectrwm-SPECTRWM_2_7_2/linux/ && make
 sudo make install
 
 ln -s $(readlink -e $current_dir) ~/.config/spectrwm
