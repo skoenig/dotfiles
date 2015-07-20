dcc8aa5972cc820bc511037ce5506b51dd338354
skoenig <soeren.koenig@mailbox.org>
2015-07-20 23:50:14 +0200

    baraction with battery status

--- configs/spectrwm/baraction.sh
+++ configs/spectrwm/baraction.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # baraction.sh for scrotwm status bar
 
+BAT_PATH=/sys/class/power_supply/BAT0
+
 while true
 do
     eval $(sensors 2>/dev/null | sed s/[°+]//g | awk '/^CPU Temperature/ {printf "CORETEMP=%s;", $3}; /^MB/ {printf "MBTEMP=%s;",$3}; /^CPU Fan Speed/ {printf "FANSPD=%s;",$4};' -)
@@ -12,8 +14,12 @@ do
     MUSED=$(( $MTOT - $MFREE ))
     MUSEDPT=$(( ($MUSED * 100) / $MTOT ))
     MEM_STR="M: ${MUSEDPT}%"
+    BAT_NOW=$(cat $BAT_PATH/energy_now)
+    BAT_FULL=$(cat $BAT_PATH/energy_full)
+    BAT=$(( ($BAT_NOW * 100) / $BAT_FULL ))
+    BAT_STATUS=$(cat $BAT_PATH/status)
 
-    echo -e ":: $TEMP_STR | $FREQ_STR | $LOAD_STR | $UPTIME_STR | $MEM_STR ::"
+    echo -e ":: $TEMP_STR | $FREQ_STR | $LOAD_STR | $UPTIME_STR | $MEM_STR | $BAT% $BAT_STATUS ::"
 
     sleep 5
 done
