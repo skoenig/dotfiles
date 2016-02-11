e991961b05bc301b3a0df711f57937f262f0a8b7
skoenig <soeren.koenig@mailbox.org>
2016-02-11 13:53:17 +0100

    switch awesomewm config based on host OS

--- configs/awesome/autostart.sh
+++ configs/awesome/autostart.sh
@@ -4,14 +4,14 @@
 wmname LG3D
 
 ## Set root window colour
-#hsetroot -solid "#333333" &
-#feh --bg-fill ~/graphic/wallpapers/nature/nature_190.png &
+hsetroot -solid "#333333" &
+feh --bg-fill ~/graphic/wallpapers/nature/nature_190.png &
 
 ## this may be also needed by nm-applet
 #/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
 ## Start daemons
-gnome-screensaver &
+#gnome-screensaver &
 ~/.dropbox-dist/dropboxd &
 
 cd ~
@@ -20,9 +20,10 @@ cd ~
 eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
-## ck-launch-session: nm-applet needs this to run w/h root privileges + installed gksu
-if [ -f $HOME/.config/awesome/rc.lua-$HOSTNAME ]
+if [[ -f /etc/debian_version && -f $HOME/.config/awesome/rc.lua-debian ]]
 then
-    config="--config $HOME/.config/awesome/rc.lua-$HOSTNAME"
+    config="--config $HOME/.config/awesome/rc.lua-debian"
 fi
-exec ck-launch-session awesome $config >> ~/.cache/awesome/stdout 2>> ~/.cache/awesome/stderr
+## ck-launch-session: nm-applet needs this to run w/h root privileges + installed gksu
+#exec ck-launch-session awesome $config >> ~/.cache/awesome/stdout 2>> ~/.cache/awesome/stderr
+exec awesome $config >> ~/.cache/awesome/stdout 2>> ~/.cache/awesome/stderr
