cf6db09ebc055ab9c46d452eda2675ad3d15df37
skoenig <soeren.koenig@mailbox.org>
2017-02-01 19:44:50 +0100

    updated config for awesome >= 4.0

--- configs/awesome/autostart.sh
+++ configs/awesome/autostart.sh
@@ -11,13 +11,13 @@ feh --bg-fill ~/graphic/wallpapers/nature/nature_190.png &
 ## this may be also needed by nm-applet
 #/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
-## GNOME PolicyKit and Keyring
-eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
-export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
-
 if [[ -f /etc/debian_version && -f $HOME/.config/awesome/rc.lua-debian ]]
 then
     config="--config $HOME/.config/awesome/rc.lua-debian"
+    if [[ $(cat /etc/debian_version) == "9.0" ]]
+    then
+        config="$config-new"
+    fi
 fi
 ## ck-launch-session: nm-applet needs this to run w/h root privileges + installed gksu
 #exec ck-launch-session awesome $config >> ~/.cache/awesome/stdout 2>> ~/.cache/awesome/stderr
