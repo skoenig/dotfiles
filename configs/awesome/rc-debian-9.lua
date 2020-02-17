4c85b3b67458bd85df1d05b79712a14d88c906ff
skoenig <soeren.koenig@mailbox.org>
2020-02-17 05:10:27 +0100

    use magnifier layout

--- configs/awesome/rc-debian-9.lua
+++ configs/awesome/rc-debian-9.lua
@@ -1,4 +1,4 @@
--- *created Tuesday, 10. December 2013 - updated Friday, 13. September 2019*
+-- *created Tuesday, 10. December 2013 - updated Sunday, 17. November 2019*
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
@@ -95,7 +95,7 @@ awful.layout.layouts = {
 --    awful.layout.suit.spiral.dwindle,
 --    awful.layout.suit.max,
 --    awful.layout.suit.max.fullscreen,
---    awful.layout.suit.magnifier,
+    awful.layout.suit.magnifier,
 --    awful.layout.suit.corner.nw,
 --    awful.layout.suit.corner.ne,
 --    awful.layout.suit.corner.sw,
